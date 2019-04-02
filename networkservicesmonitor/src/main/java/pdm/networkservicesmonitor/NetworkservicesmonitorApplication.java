191f0dfeb6587b62b3b21828bfd8d0fb77dc2da4
Patryk Milewski <patryk.milewski@protonmail.com>
2019-04-02 21:44:19 +0200

    Init commit

---
+++ networkservicesmonitor/src/main/java/pdm/networkservicesmonitor/NetworkservicesmonitorApplication.java
@@ -0,0 +1,13 @@
+package pdm.networkservicesmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkservicesmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkservicesmonitorApplication.class, args);
+	}
+
+}
